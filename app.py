6ae52c94de539e82978a7749e6ca04707f0ab7d9
pmaribojoc <pmaribojoc@captechconsulting.com>
2021-05-30 15:35:07 -0400

    added initial yfinance integration

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 # app.py
+import yfinance
+from flask import Flask, request
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/stock/<ticker>")
+def data(ticker):
+    ticker == request.view_args['ticker']
+    try:
+
+        print (yfinance.Ticker(ticker).history(period="5d", interval="1h"))
+        return ticker
+    except:
+        return 'Invalid stock ticker'
